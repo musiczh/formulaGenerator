9df9e54eefda9e46a4d16d0bd93a75d070f68725
huan <172802065@qq.com>
2020-03-29 19:52:51 +0800

    增加生成式子模块

--- src/Generation.java
+++ src/Generation.java
@@ -1,36 +1,95 @@
+import inter.GenerateInter;
+
 import java.util.Random;
+import java.util.regex.Pattern;
 
 /**
  * 负责生成题目的类
  */
-public class Generation {
+public class Generation implements GenerateInter {
 
+    @Override
     public String getFormula(int bound){
-        int operatorNum = new Random().nextInt(2)+1;
+        int operatorNum = getNotZeroIntRandom(3);
         StringBuilder stringBuilder = new StringBuilder();
         for (int i=0;i<2*operatorNum+1;i++){
             if (i%2==0){
-
+                addNum(stringBuilder,bound);
             }else{
-
+                addOperator(stringBuilder);
             }
         }
+        addBrackets(stringBuilder,operatorNum);
         return stringBuilder.toString();
     }
 
 
 
     private void addNum(StringBuilder stringBuilder, int bound){
-        int numType = new Random().nextInt(2);
-        if (numType<1){
-
+        int numType = getNotZeroIntRandom(3);
+        if (numType<=1){
+            stringBuilder.append(getNotZeroIntRandom(bound))
+                    .append("/")
+                    .append(getNotZeroIntRandom(bound));
         }else{
-
+            stringBuilder.append(getNotZeroIntRandom(bound))
+                    .append("/1");
         }
     }
 
     private void addOperator(StringBuilder stringBuilder){
+        int type = getNotZeroIntRandom(4);
+        switch (type){
+            case 1: stringBuilder.append(" + "); break;
+            case 2: stringBuilder.append(" - "); break;
+            case 3: stringBuilder.append(" × "); break;
+            case 4: stringBuilder.append(" ÷ "); break;
+            default:break;
+        }
+    }
 
+    private void addBrackets(StringBuilder stringBuilder,int operatorNum){
+        if (operatorNum==2){
+
+            int index = getNotZeroIntRandom(2);
+            if (index==1){
+                stringBuilder.insert(0,"( ");
+                stringBuilder.insert(11," )");
+            }
+            else{
+                stringBuilder.insert(6,"( ");
+                stringBuilder.insert(stringBuilder.length()," )");
+            }
+
+        }else if(operatorNum==3){
+            int index = getNotZeroIntRandom(3);
+            switch (index){
+                case 1:
+                    stringBuilder.insert(0,"( ");
+                    switch (getNotZeroIntRandom(3)){
+                        case 1:stringBuilder.insert(11," )"); break;
+                        case 2:stringBuilder.insert(17," )"); break;
+                        case 3:stringBuilder.insert(stringBuilder.length()," )"); break;
+                    }
+                    break;
+                case 2:
+                    stringBuilder.insert(6,"( ");
+                    if (getNotZeroIntRandom(2)==1) stringBuilder.insert(17," )");
+                    else stringBuilder.insert(stringBuilder.length()," )");
+                    break;
+                case 3:
+                    stringBuilder.insert(12,"( ");
+                    stringBuilder.insert(stringBuilder.length()," )");
+                    break;
+                default:break;
+            }
+        }
+
+
+    }
+
+    private int getNotZeroIntRandom(int bound){
+        return new Random().nextInt(bound)+1;
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
+        Generation generation = new Generation();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i=0;i<=100;i++){
+            System.out.println(generation.getFormula(9));
+        }
 
     }
+
+
 }
